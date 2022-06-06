db9a1c0a6f146b518091395f37d3d1040378a8ea
Juan José Angel <angel.juan@correounivalle.edu.co>
2022-06-06 11:26:24 -0500

    logging 1

--- get_data.py
+++ get_data.py
@@ -2,6 +2,7 @@ from datetime import date
 import os 
 import requests
 import json 
+import logging
 
 def download_data(data:str):
     """
@@ -18,13 +19,19 @@ def download_data(data:str):
     path=os.path.join("data",data,str(year)+"-"+month)
     #test if the data already exists
     if os.path.isfile(os.path.join(path, data+"-"+today.strftime("%b-%d-%Y")+".csv")):  
+        logging.debug ("Ya hay un archivo con la misma fecha, voy a eliminar el archivo.")
         os.remove(os.path.join(path, data+"-"+today.strftime("%b-%d-%Y")+".csv"))
-    else: 
+    elif not os.path.isdir(path): 
+        logging.debug ("creando path") 
         os.makedirs(path)
     #download and write data
     w=open(os.path.join(path,data+"-"+today.strftime("%b-%d-%Y")+".csv"),"bw+")
-    r=requests.get(urls[data],allow_redirects=True)
-    w.write(r.content)
+    logging.debug ("descargando la información de "+ urls[data])
+    try:
+        r=requests.get(urls[data],allow_redirects=True)
+        w.write(r.content)
+    except ValueError:
+        logging.error("error descargando el archivo")
 
 
 download_data("museos")
